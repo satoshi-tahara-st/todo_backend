b1a52fa6df559a780732a9e59a3db97a7505e824
satoshi-tahara-st <sts3104mnr@gmail.com>
2024-01-13 17:42:45 +0900

    first commit

---
+++ _tool/db/init/init_todo.sql
@@ -0,0 +1,24 @@
+DROP DATABASE IF EXISTS todo_database;
+CREATE DATABASE todo_database;
+USE todo_database;
+
+CREATE TABLE IF NOT EXISTS `user_t` (
+    `user_id` varchar(128) NOT NULL,
+    `user_name` varchar(128) NOT NULL,
+    `password` varchar(128) NOT NULL,
+    `deleted_time` datetime,
+    `register_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSEt=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS `todo_t` (
+    `todo_id` varchar(128) NOT NULL,
+    `user_id` varchar(128) NOT NULL,
+    `todo` TEXT NOT NULL,
+    `deleted_time` datetime,
+    `register_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`todo_id`, `user_id`),
+    FOREIGN KEY (`user_id`) REFERENCES `user_t`(`user_id`)
+) ENGINE=InnoDB DEFAULT CHARSEt=utf8mb4;
